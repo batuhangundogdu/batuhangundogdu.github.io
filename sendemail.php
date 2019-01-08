3d312f131956243206b03941fe9b202c8b6f9594
Batuhan Gündoğdu <batuhan.gundogdu@stu.fsm.edu.tr>
2019-01-08 23:46:20 +0300

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,12 +1,12 @@
 <?php
-  echo ("i'm in php");
+  echo "i'm in php";
 	header('Content-type: application/json');
 	$status = array(
 		'type'=>'success',
 		'message'=>'Thank you for contact us. As early as possible  we will contact you '
   );
 
-  echo ("status -> $status");
+  echo "status : " . $status;
 
     $name = @trim(stripslashes($_POST['name']));
     $email = @trim(stripslashes($_POST['email']));
@@ -18,13 +18,13 @@
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
-    echo ("body -> $body");
+    echo "body : " . $body;
 
     if(@mail($email_to, $subject, $body, 'From: <'.$email_from.'>')) {
-      echo ("mail success php");
+      echo "mail success php";
       echo json_encode($status);
     } else {
-      echo ("mail ERROR php");
+      echo "mail ERROR php";
       echo json_encode(['success'=>false]);
     }
     die;
